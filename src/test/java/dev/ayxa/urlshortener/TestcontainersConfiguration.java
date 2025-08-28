64a3cfded12b307aeb858769d20e208056996931
Ayxa Chaverra <achaverrar@gmail.com>
2025-08-28 01:09:26 -0500

    use test-containers on CI pipeline

--- src/test/java/dev/ayxa/urlshortener/TestcontainersConfiguration.java
+++ src/test/java/dev/ayxa/urlshortener/TestcontainersConfiguration.java
@@ -1,35 +1,22 @@
-/* package dev.ayxa.urlshortener;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-@TestConfiguration(proxyBeanMethods = false)
-@Testcontainers
-class TestcontainersConfiguration {
-
-	@Container
-	@ServiceConnection
-	private static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17.5-alpine");
-
-} */
 package dev.ayxa.urlshortener;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+	@Container
+	private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17.5-alpine");
 
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.5-alpine"));
+		return postgresContainer;
 	}
-
 }
